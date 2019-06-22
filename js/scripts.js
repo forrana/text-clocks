027c05ac0204cb54a7b5b16755695a686767e987
Aliaksei Kuchyn <forrana@gmail.com>
2019-06-22 21:35:56 +0300

    Change design and logic to make more informal clocks

--- js/scripts.js
+++ js/scripts.js
@@ -2,102 +2,133 @@ const TIME_FORMAT = { hour: 'numeric', minute: 'numeric', hour12: true };
 
 const timeElement = document.querySelector("#time");
 
-const numbersToWordsMatrix = {
-    0: null,
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-    20: "twenty",
-    30: "thirty",
-    40: "forty",
-    50: "fifty",
-    60: "sixty",
-};
+const minutesWordsMatrix = new Map([
+  [ 20, "twenty" ],
+  [ 5, "five" ],
+  [ 10, "ten" ],
+  [ 15, "quarter" ],
+  [ 30, "half" ],
+  [ 99, "almost" ],
+])
 
-const numbersToWords = (number) => {
-    let minutesWord = "";
-    if (number <= 20) {
-        minutesWord = number === 15 ?
-            "quarter" :
-            numbersToWordsMatrix[number];
-    } else {
-        const minutesDigit = number % 10;
-        const minutesTens = number - minutesDigit;
-        minutesWord = number === 30 ?
-            "half" :
-            ` ${numbersToWordsMatrix[minutesTens]} ${numbersToWordsMatrix[minutesDigit]}`
-    }
-    return minutesWord;
-};
+const boundingsWordsMatrix = new Map([
+  [ "past", "past" ],
+  [ "to", "to" ],
+])
 
-const minutesToWords = (minutes) => {
-    minutes = +minutes;
-    if(!minutes) return null;
-    if (minutes <= 30) {
-        return `${numbersToWords(minutes)} past`
-    } else {
-        return `${numbersToWords(60 - minutes)} to`
-    }
-};
+const hoursWordsMatrix = new Map([
+  [ 1, "one" ],
+  [ 2, "two" ],
+  [ 3, "three" ],
+  [ 4, "four" ],
+  [ 5, "five" ],
+  [ 6, "six" ],
+  [ 7, "seven" ],
+  [ 8, "eight" ],
+  [ 9, "nine" ],
+  [ 10, "ten" ],
+  [ 11, "eleven" ],
+  [ 12, "twelve" ],
+  [ "oclock", "O\'Clock" ],
+  ])
 
-const hoursToWords = (hours) => {
-    hours = +hours;
-    return numbersToWords(hours);
-};
+const generateWords = (matrix, elementId) => {
+  const matrixBlock = document.querySelector(`#words`);
+  matrix.forEach( (value, key) => {
+    let span = document.createElement('span');
+    span.id = `${elementId}-${key}`;
+    span.innerHTML = value;
+    matrixBlock.appendChild(span);
+  })
+}
 
-const timeToWords = (hours, minutes, isAM) => {
-    if(!(+minutes)) {
-        if(hours === "12") {
-            return isAM ? "Midnight" : "Midday";
-        } else return `${hoursToWords(hours)} O\'clock`;
-    } else {
-        if(minutes > 30){
-            hours = +hours + 1;
-        }
-        return `${minutesToWords(minutes)} ${hoursToWords(hours)}`
-    }
-};
+const resetActiveElements = () => {
+  const activeElements = document.querySelectorAll('.active');
+  activeElements.forEach( (element) => element.classList.toggle('active'));
+}
 
-let previousTime = ""
+const markCurrentHour = (hour, isLessThenHalf) => {
+  hour = !isLessThenHalf ? hour + 1 : hour;
+  const currentHourElement = document.querySelector(`#hours-${hour}`);
+  currentHourElement.classList.add("active");
+}
 
-const printCurrentTime = (date) => {
-    let time = date.toLocaleString('en-US', TIME_FORMAT);
-    if (time.length === 7) {
-      time = `0${time}`;
-    }
-    const minutes = time.slice(3, 5);
-    const hours = time.slice(0, 2);
-    const isAM = time.slice(6,8) === "AM";
-    const newTime = `${timeToWords(hours, minutes, isAM)} ${time.slice(6,8)}`;
-    if (newTime !== previousTime) {
-      timeElement.innerHTML = newTime;
-      previousTime = newTime;
-    }
+const markCurrentBinding = (minute, isLessThenHalf) => {
+  if(!isLessThenHalf && minute < 57) {
+    const currentBoundingElement = document.querySelector(`#boundings-to`);
+    currentBoundingElement.classList.add("active");
+  } else if(minute > 3 && minute < 57) {
+    const currentBoundingElement = document.querySelector(`#boundings-past`);
+    currentBoundingElement.classList.add("active");
+  }
+}
 
-};
+const markCurrentMinute = (minute, isLessThenHalf) => {
+  let currentMinuteElement = "";
+  minute = isLessThenHalf ? minute : 60 - minute;
+  console.log(minute, isLessThenHalf)
+  switch (true) {
+    case minute > 0 && minute <= 3:
+      currentMinuteElement = document.querySelector(`#hours-oclock`);
+      currentMinuteElement.classList.add("active");
+      if (!isLessThenHalf) {
+        currentMinuteElement = document.querySelector(`#minutes-99`);
+        currentMinuteElement.classList.add("active");
+      }
+      break;
+    case minute > 3 && minute <= 7:
+      currentMinuteElement = document.querySelector(`#minutes-5`);
+      currentMinuteElement.classList.add("active");
+      break;
+    case minute > 7 && minute <= 12:
+      currentMinuteElement = document.querySelector(`#minutes-10`);
+      currentMinuteElement.classList.add("active");
+      break;
+    case minute > 12 && minute <= 17:
+      currentMinuteElement = document.querySelector(`#minutes-15`);
+      currentMinuteElement.classList.add("active");
+      break;
+    case minute > 17 && minute <= 22:
+      currentMinuteElement = document.querySelector(`#minutes-20`);
+      currentMinuteElement.classList.add("active");
+      break;
+    case minute > 22 && minute <= 27:
+      currentMinuteElement = document.querySelector(`#minutes-20`);
+      currentMinuteElement.classList.add("active");
 
-printCurrentTime(new Date());
+      currentMinuteElement = document.querySelector(`#minutes-5`);
+      currentMinuteElement.classList.add("active");
+      break;
+    case minute > 27 && minute <= 32:
+      currentMinuteElement = document.querySelector(`#minutes-30`);
+      currentMinuteElement.classList.add("active");
+      break;
+  }
+}
 
-setInterval(() => printCurrentTime(new Date()), 1000*10);
+const markCurrentTime = (date) => {
+  resetActiveElements();
+  let time = date.toLocaleString('en-US', TIME_FORMAT);
+  if (time.length === 7) {
+    time = `0${time}`;
+  }
+  let minute = Number(time.slice(3, 5));
+  let hour = Number(time.slice(0, 2));
+  let isLessThenHalf = minute > 32 ? false : true;
+  markCurrentHour(hour, isLessThenHalf);
+  markCurrentBinding(minute, isLessThenHalf);
+  markCurrentMinute(minute, isLessThenHalf);
+}
 
-document.addEventListener( 'visibilitychange' , () => {
-    if (!document.hidden) {
-        printCurrentTime(new Date());
-    }
-}, false );
+generateWords(minutesWordsMatrix, "minutes");
+generateWords(boundingsWordsMatrix, "boundings");
+generateWords(hoursWordsMatrix, "hours");
+//markCurrentTime(new Date())
+
+//setInterval(() => markCurrentTime(new Date()), 1000*20);
+
+// document.addEventListener( 'visibilitychange' , () => {
+//     if (!document.hidden) {
+//         markCurrentTime(new Date());
+//     }
+// }, false );
