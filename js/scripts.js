b3d0f8e8449181b0a9d2ec19345dc758dc743265
Aliaksei Kuchyn <forrana@gmail.com>
2019-06-25 23:58:37 +0300

    Refactoring: move logic to separate files, markCurrentTime logic, add tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,12 @@
-const TIME_FORMAT = { hour: 'numeric', minute: 'numeric', hour12: true };
-
-const timeElement = document.querySelector("#time");
-
-const minutesWordsMatrix = new Map([
-  [ 20, "twenty" ],
-  [ 5, "five" ],
-  [ 10, "ten" ],
-  [ 15, "quarter" ],
-  [ 30, "half" ],
-  [ 99, "almost" ],
-])
-
-const boundingsWordsMatrix = new Map([
-  [ "past", "past" ],
-  [ "to", "to" ],
-])
-
-const hoursWordsMatrix = new Map([
-  [ 1, "one" ],
-  [ 2, "two" ],
-  [ 3, "three" ],
-  [ 4, "four" ],
-  [ 5, "five" ],
-  [ 6, "six" ],
-  [ 7, "seven" ],
-  [ 8, "eight" ],
-  [ 9, "nine" ],
-  [ 10, "ten" ],
-  [ 11, "eleven" ],
-  [ 12, "twelve" ],
-  [ "oclock", "O\'Clock" ],
-  ])
+import runTests from "./tests.js";
+import {
+  TIME_FORMAT,
+  TIME_DELTA,
+  hoursWordsMatrix,
+  boundingsWordsMatrix,
+  minutesWordsMatrix,
+  timeElement,
+} from "./consts.js";
 
 const generateWords = (matrix, elementId) => {
   const matrixBlock = document.querySelector(`#words`);
@@ -51,24 +27,32 @@ const markCurrentHour = (hour, isLessThenHalf) => {
   hour = !isLessThenHalf ? (hour < 12 ? hour + 1 : 1) : hour;
   const currentHourElement = document.querySelector(`#hours-${hour}`);
   currentHourElement.classList.add("active");
+  return currentHourElement.innerText;
 }
 
 const markCurrentBinding = (minute, isLessThenHalf) => {
-  if(!isLessThenHalf && minute < 57) {
-    const currentBoundingElement = document.querySelector(`#boundings-to`);
+  let currentBoundingElement = "";
+  if(minute <= TIME_DELTA || minute + TIME_DELTA >= 60) return;
+  if (isLessThenHalf) {
+    currentBoundingElement = document.querySelector("#boundings-past");
     currentBoundingElement.classList.add("active");
-  } else if(minute > 3 && minute < 57) {
-    const currentBoundingElement = document.querySelector(`#boundings-past`);
+  } else {
+    currentBoundingElement = document.querySelector("#boundings-to");
     currentBoundingElement.classList.add("active");
   }
+
+  return currentBoundingElement.innerText;
 }
 
 const markCurrentMinute = (minute, isLessThenHalf) => {
   let currentMinuteElement = "";
   minute = isLessThenHalf ? minute : 60 - minute;
-  console.log(minute, isLessThenHalf)
-  switch (true) {
-    case minute > 0 && minute <= 3:
+  // round to closest minute
+  // 3 and 7 = 5
+  // 8 and 12 = 10
+  const closestMinute = Math.round(minute / 5) * 5
+  switch (closestMinute) {
+    case 0:
       currentMinuteElement = document.querySelector(`#hours-oclock`);
       currentMinuteElement.classList.add("active");
       if (!isLessThenHalf) {
@@ -76,54 +60,44 @@ const markCurrentMinute = (minute, isLessThenHalf) => {
         currentMinuteElement.classList.add("active");
       }
       break;
-    case minute > 3 && minute <= 7:
+    case 25:
+      currentMinuteElement = document.querySelector(`#minutes-20`);
+      currentMinuteElement.classList.add("active");
       currentMinuteElement = document.querySelector(`#minutes-5`);
       currentMinuteElement.classList.add("active");
       break;
-    case minute > 7 && minute <= 12:
-      currentMinuteElement = document.querySelector(`#minutes-10`);
+    default:
+      currentMinuteElement = document.querySelector(`#minutes-${closestMinute}`);
       currentMinuteElement.classList.add("active");
-      break;
-    case minute > 12 && minute <= 17:
-      currentMinuteElement = document.querySelector(`#minutes-15`);
-      currentMinuteElement.classList.add("active");
-      break;
-    case minute > 17 && minute <= 22:
-      currentMinuteElement = document.querySelector(`#minutes-20`);
-      currentMinuteElement.classList.add("active");
-      break;
-    case minute > 22 && minute <= 27:
-      currentMinuteElement = document.querySelector(`#minutes-20`);
-      currentMinuteElement.classList.add("active");
-
-      currentMinuteElement = document.querySelector(`#minutes-5`);
-      currentMinuteElement.classList.add("active");
-      break;
-    case minute > 27 && minute <= 32:
-      currentMinuteElement = document.querySelector(`#minutes-30`);
-      currentMinuteElement.classList.add("active");
-      break;
   }
+  return currentMinuteElement.innerText;
 }
 
-const markCurrentTime = (date) => {
-  resetActiveElements();
+const getTimeFromDateString = (date) => {
   let time = date.toLocaleString('en-US', TIME_FORMAT);
   if (time.length === 7) {
     time = `0${time}`;
   }
   let minute = Number(time.slice(3, 5));
   let hour = Number(time.slice(0, 2));
-  let isLessThenHalf = minute > 32 ? false : true;
-  markCurrentHour(hour, isLessThenHalf);
-  markCurrentBinding(minute, isLessThenHalf);
-  markCurrentMinute(minute, isLessThenHalf);
+  return { minute, hour };
+}
+
+const markCurrentTime = (date) => {
+  resetActiveElements();
+  const { minute, hour } = getTimeFromDateString(date);
+  let isLessThenHalf = minute > 30 + TIME_DELTA ? false : true;
+  const currentHour = markCurrentHour(hour, isLessThenHalf);
+  const currentBinding = markCurrentBinding(minute, isLessThenHalf);
+  const currentMinute = markCurrentMinute(minute, isLessThenHalf);
+  return { currentMinute, currentBinding, currentHour };
 }
 
 generateWords(minutesWordsMatrix, "minutes");
 generateWords(boundingsWordsMatrix, "boundings");
 generateWords(hoursWordsMatrix, "hours");
-markCurrentTime(new Date())
+runTests(markCurrentTime);
+markCurrentTime(new Date());
 
 setInterval(() => markCurrentTime(new Date()), 1000*20);
 
